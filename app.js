a49ebe93546d64ebabafa22e377446ef6b0cb75a
Sungwon Ryu <swryu0906@gmail.com>
2015-12-04 18:12:43 -0500

    Set up the basic express configuration in app.js
    Configurations in app.js
    . dependencies
    . path - public
    . middleware - logger, bodyParser, cookieParser
    . root route
    . server

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict'
+
+
+/**
+ * dependencies
+ */
+
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let expressValidator = require('express-validator');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let flash = require('connect-flash');
+let session = require('express-session');
+let passport = require('passport');
+let mongoose = require('mongoose');
+
+let app = express();
+
+
+/**
+ * path configuration
+ */
+
+app.set(express.static(path.join(__dirname, 'public')));
+
+
+/**
+ * parser configuration
+ */
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+/**
+ * root route
+ */
+
+app.get('/', (req, res) => {
+  res.send('Instagram App');
+})
+
+
+/**
+ * server configuration
+ */
+
+let server = app.listen(process.env.PORT || 3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Instagram App is running', 'host: ' + host, 'port: ' + port);
+});
